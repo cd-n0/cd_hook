2a4eac49aab03a6eac2a419c2bb8b113a2a91bb2
cd-n0 <59338209+cd-n0@users.noreply.github.com>
2024-11-19 14:08:42 +0000

    Removed the "jmp" directory that had asm instructions I wrote as a reference for inline hooking

--- jmp/jmp32.asm
+++
@@ -1,5 +0,0 @@
-    global _start
-    section .text
-_start:
-    mov eax, 0x01ffffff ; i386: b8 ff ffff 01
-    jmp eax ; i386: ff e0
--- jmp/jmp64.asm
+++
@@ -1,5 +0,0 @@
-    global _start
-    section .text
-_start:
-    mov rax, 0x01ffffffffffffff ; x86_64: 48 b8 ffff ffff ffff ff01
-    jmp rax ; x86_64: ff e0
