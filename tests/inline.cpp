053eb0739cba91ca7eebd43982dbde788181717e
cdn0 <59338209+cd-n0@users.noreply.github.com>
2025-01-24 17:22:14 +0300

    Fix inline hooking tests

--- tests/inline.cpp
+++ tests/inline.cpp
@@ -1,16 +1,20 @@
 // TODO: Something doesn't work when optimizations are on
-extern "C" {
 #include "cd_hook.h"
-}
 #include <assert.h>
 #define UNUSED(x) (void)x
 #define BUFFER_SIZE 2048
 
 char out[BUFFER_SIZE];
 
+__attribute__((noinline)) void f1(void);
+__attribute__((noinline)) void f2(void);
+__attribute__((noinline)) int f3(int a, int b);
+__attribute__((noinline)) int f4(int a, int b);
+
 void f1(void){
     strcpy(out, "f1");
 }
+
 void f2(void){
     strcpy(out, "Hooked by f2");
 }
@@ -20,27 +24,33 @@ int f3(int a, int b){
 }
 
 int f4(int a, int b){
-    UNUSED(a);
-    UNUSED(b);
-    return 42;
+    return 10 + a + b;
 }
 
 int main(void){
-    cd_hook_ctx *f1_ctx = ch_create_ctx((void*)f1, (void*)f2);
     f1();
     assert(0 == strcmp(out, "f1"));
+
+    cd_hook_ctx *f1_ctx = ch_create_ctx((void*)f1, (void*)f2);
     ch_inline(f1_ctx, true);
     f1();
     assert(0 == strcmp(out, "Hooked by f2"));
+
     ch_inline(f1_ctx, false);
     f1();
     assert(0 == strcmp(out, "f1"));
+
+    assert(15 == f3(5, 10));
+
     cd_hook_ctx *f3_ctx = ch_create_ctx((void*)f3, (void*)f4);
-    assert(15 == f3(5, 10));
     ch_inline(f3_ctx, true);
-    assert(42 == f3(5, 10));
+    assert(42 == f3(12, 20));
+
     ch_inline(f3_ctx, false);
-    assert(15 == f3(5, 10));
+    assert(25 == f3(5, 20));
+
     ch_destroy_ctx(f1_ctx);
     ch_destroy_ctx(f3_ctx);
+
+    return 0;
 }
