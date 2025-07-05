592059a20cff93bd8776d1b70660c2e3476af4dc
cdn0 <59338209+cd-n0@users.noreply.github.com>
2025-07-05 19:34:54 +0300

    Noinline attribute for the clang macro too, just in case.

--- tests/test_common.h
+++ tests/test_common.h
@@ -2,7 +2,7 @@
 #define TEST_COMMON_H_
 #if defined(_MSC_VER)
 #define NO_INLINE __declspec(noinline)
-#elif defined(__GNUC__)
+#elif defined(__GNUC__) || defined(__clang__)
 #define NO_INLINE __attribute__((noinline))
 #endif
 #endif // !TEST_COMMON_H_
